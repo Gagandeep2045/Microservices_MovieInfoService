0a9d4a6049cdf837904943ea773fa72539271c86
GagandeepBudhiraja <gagandeep.singh2045@gmail.com>
2019-11-03 23:41:31 +0530

    Distributed log tracing using Sleuth

--- src/main/java/javabrains/io/movieInfo/controller/MovieController.java
+++ src/main/java/javabrains/io/movieInfo/controller/MovieController.java
@@ -3,6 +3,8 @@ package javabrains.io.movieInfo.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,12 @@ import javabrains.io.movieInfo.model.Movie;
 @RequestMapping("/movies")
 public class MovieController {
 
+	private Logger logger=LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("/info")
 	public Movie getMovies(@RequestParam()String movieId)
 	{
-		
+		logger.info("Delegating request to service class");
 		return Arrays.<Movie>asList(new Movie("M101","Titanic","English Movie"),
 				new Movie("M102","Sholeey","Hindi Movie"),
 				new Movie("M103","Spiderman","English Movie")
